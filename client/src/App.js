5096d95faebf959e4a2c5d8ca83f05034c871913
aLVINlEE9 <fldh3746@gmail.com>
2022-09-26 16:58:13 +0900

    head nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import Home from './pages/home/Home';
-import UserPage from './pages/myProfile/MyProfile';
-import NotFound from './pages/notFound/NotFound';
-import Market from './pages/market/Market';
-import './App.css';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import PostDetail from './components/postDetail/PostDetail';
-import Posting from './pages/posting/Posting';
-import { AuthContext } from './context/AuthContext';
-import { useContext } from 'react';
+import React from "react";
+import Home from "./pages/home/Home";
+import UserPage from "./pages/myProfile/MyProfile";
+import NotFound from "./pages/notFound/NotFound";
+import Market from "./pages/market/Market";
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import PostDetail from "./components/postDetail/PostDetail";
+import Posting from "./pages/posting/Posting";
+import Main from "./components/main/Main";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts/:id" element={ <PostDetail /> } />
-        <Route path="/myprofile" element={user ? <UserPage /> : <Navigate to="/" />} />
-        <Route path="/market" element={user ? <Market /> : <Navigate to="/" />} />
-        <Route path="/posting" element={user ? <Posting /> : <Navigate to="/" />} />
-        <Route path="*" element={ <NotFound /> } />
-      </Routes>
+      <div className="mainWrapper">
+        <div className="mainLeft">
+          <Header />
+        </div>
+        <div className="mainRight">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route
+              path="/myprofile"
+              element={user ? <UserPage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/market"
+              element={user ? <Market /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/posting"
+              element={user ? <Posting /> : <Navigate to="/" />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+
       <Footer />
     </BrowserRouter>
   );
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,38 +5,6 @@ import styles from './Footer.module.scss'
 export default function Footer() {
   return (
     <footer className={styles.container}>
-      <div className={styles.inner}>
-        <div className={styles.informationContainer}>
-          <div className={styles.information}>
-            <span className={styles.logo}>Klayklay</span>
-            <p className={styles.description}>
-              descriptiondescriptiondescription <br />
-              descriptiondescriptiondescription <br />
-              descriptiondescriptiondescription <br />
-              descriptiondescriptiondescription <br />
-              descriptiondescriptiondescription
-            </p>
-          </div>
-        </div>
-        <div className={styles.linkContainer}>
-          <div className={styles.linkItem}>
-            <span className={styles.linkTitle}>Title</span>
-            <Link to="/">Link</Link>
-          </div>
-          <div className={styles.linkItem}>
-            <span className={styles.linkTitle}>Title</span>
-            <Link to="/">Link</Link>
-          </div>
-          <div className={styles.linkItem}>
-            <span className={styles.linkTitle}>Title</span>
-            <Link to="/">Link</Link>
-          </div>
-          <div className={styles.linkItem}>
-            <span className={styles.linkTitle}>Title</span>
-            <Link to="/">Link</Link>
-          </div>
-        </div>
-      </div>
       <div className={styles.copyrightContainer}>
         <div className={styles.copyright}>Klayklay ⓒ{ new Date().getFullYear() } All Right Resolved.</div>
       </div>
